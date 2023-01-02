988a0868cb01812634e2dd29086122f79502892e
Rando Toming <rtoming@gmail.com>
2023-01-02 03:45:43 +0200

    classes - [created classes]

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+class Board {
+
+}
+
+class Player {
+
+}
+
+class Game {
+
+}
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
