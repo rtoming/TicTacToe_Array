2a7f36944510389344b5d743c7356057becaf0ea
Rando Toming <rtoming@gmail.com>
2023-01-02 04:16:27 +0200

    classes - [updated Player class]

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 class Board {
     String[] board = {" "," "," "," "," "," "," "," "," "};
+    int position;
+    int count;
 
     void printBoard() {
         System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + " ");
@@ -9,6 +11,38 @@ class Board {
         System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5] + " ");
         System.out.println("---+---+---");
         System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] + " ");
+    }
+
+    boolean fillBoard() {
+        if (board[position - 1].equals(" ")) {
+            board[position - 1] = "markIt";
+            count++;
+            return true;
+        } else {
+            System.out.println("Choosed position is occupied, take another one...");
+            return false;
+        }
+    }
+
+    boolean checkWinner(String marked) {
+        if (
+                (board[0].equals(marked) && board[1].equals(marked) && board[2].equals(marked)) ||
+                (board[3].equals(marked) && board[4].equals(marked) && board[5].equals(marked)) ||
+                (board[6].equals(marked) && board[7].equals(marked) && board[8].equals(marked)) ||
+                (board[0].equals(marked) && board[3].equals(marked) && board[6].equals(marked)) ||
+                (board[1].equals(marked) && board[4].equals(marked) && board[7].equals(marked)) ||
+                (board[2].equals(marked) && board[5].equals(marked) && board[8].equals(marked)) ||
+                (board[0].equals(marked) && board[4].equals(marked) && board[8].equals(marked)) ||
+                (board[2].equals(marked) && board[4].equals(marked) && board[6].equals(marked))
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean checkDraw() {
+
     }
 }
 
@@ -30,6 +64,11 @@ class Player {
 }
 
 class Game {
+    String message = "Enter Your position (1 - 9)";
+
+    void play() {
+
+    }
 
 }
 
