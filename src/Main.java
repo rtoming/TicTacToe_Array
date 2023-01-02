68abf97e93571e4c00818d4a3bf611d96d6cc278
Rando Toming <rtoming@gmail.com>
2023-01-02 03:53:14 +0200

    classes - [created Board class]

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 class Board {
-
+    String[] board = {" "," "," "," "," "," "," "," "," "};
+    void printBoard() {
+        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + " ");
+        System.out.println("---+---+---");
+        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5] + " ");
+        System.out.println("---+---+---");
+        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] + " ");
+    }
 }
 
 class Player {
@@ -14,6 +21,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        new Board().printBoard();
 
     }
 }
